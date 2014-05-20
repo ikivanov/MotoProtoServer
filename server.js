da110983f8d6c918963b27a2a6ff8c6379600d00
ikivanov <ikivanov@gmail.com>
2014-05-20 15:49:49 +0300

    server provides real data from mongodb

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
+/*
+db schema:
+
+users
+{
+	id,
+	username,
+	password,
+	following: [userId1, userId2, userId3], 
+	posts: 
+	[
+		id: ObjectId(),
+		date: new Date(),
+		title: "",
+		description: "",
+		route: 
+		[
+			{lat: 1, lon: 1}
+		],
+		pics: 
+		[
+			{}
+		]
+	]
+}
+
+*/
+
 var express = require('express');
+var bodyParser = require('body-parser');
+var crypto = require('crypto');
+
 var app = module.exports = express();
 
-var currentToken = 1;
+app.use(bodyParser());
+
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+
+var connectionString = "mongodb://localhost:27017/motoProto";
+
 var token2username = {};
 
 app.get('/register', function(req, res){
@@ -10,52 +47,94 @@ app.get('/register', function(req, res){
 app.get('/logout', function(req, res){
 });
 
+var loggedUsers = {};
+
 app.post('/login', function(req, res){
-	var data = req.body.user;
-	var username = data.name;
-	var password = data.password;
+	var username = req.body.username;
+	var password = req.body.password;
 	
-	//call to User middlesware which connects to DB
-	
-	if (username === "ivan" && password === "123") {
-		//res.session.uid = "123" ???????????
-		
-		res.send({
-			success : true,
-			userToken: currentToken++,
-			msg : "OK",
+	MongoClient.connect(connectionString, function(err, db) {
+		var passwordHash = crypto.createHash('md5').update(password).digest('hex');
+		db.collection("users").findOne({username : username, password: passwordHash}, function(err, user) {
+			if (user) {
+				var userToken = crypto.randomBytes(20).toString('hex');
+				var userId = user._id.toString();
+				loggedUsers[userToken] = userId;
+				
+				res.send({
+					success : true,
+					userToken: userToken,
+					msg : "OK",
+				});
+			} else {
+				res.send({                
+					success : false, 
+					msg: "Authentication failed! Bad username or password"
+				});
+			}
+			
+			db.close();
 		});
-	} else {
-		res.send({                
-			success : false, 
-			msg: "Authentication failed! Bad username or password"
-		});
-	}
+	});
 });
 
+//gets all user's posts plus all the posts of the users he follows
+//TODO: should get portions, not all at once
 app.get('/:userToken/posts', function(req, res) {
 	var userToken = req.params.userToken;
-	console.log(userToken);
 	
-	if (!token2username[userToken]) {
+	if (!loggedUsers[userToken]) {
 		res.send({                
 			success : false, 
 			msg: "Invalid user token"
 		});
 		return;
 	}
+	
+	MongoClient.connect(connectionString, function(err, db) {
+		var userId = loggedUsers[userToken];
+		var result = [];
+		
+		db.collection("users").findOne({_id : new ObjectID(userId)}, function(err, user) {
+			var idSet = [];
+			idSet.push(user._id);
+			
+			var following = user.following;
+			for (var i = 0; i < following.length; i++) {
+				idSet.push(following[i]);
+			}
 
-	var result = {
-		success: true,
-        msg: "OK",
-		posts:[
-			{username: "ikivanov", id: users[0].posts[2].id, date: users[0].posts[2].date, title: users[0].posts[2].title, description: users[0].posts[2].description, route: users[0].posts[2].route, pics: users[0].posts[2].pics},
-			{username: "ikivanov", id: users[0].posts[1].id, date: users[0].posts[1].date, title: users[0].posts[1].title, description: users[0].posts[1].description, route: users[0].posts[1].route, pics: users[0].posts[1].pics},
-			{username: "vlao85", id: users[1].posts[1].id, date: users[1].posts[1].date, title: users[1].posts[1].title, description: users[1].posts[1].description, route: users[1].posts[1].route, pics: users[1].posts[1].pics},
-			{username: "kolevstefan", id: users[3].posts[1].id, date: users[3].posts[1].date, title: users[3].posts[1].title, description: users[3].posts[1].description, route: users[3].posts[1].route, pics: users[3].posts[1].pics},
-		]
-	};
-	res.send(result);
+			db.collection("users").find({_id: {$in: idSet}}).toArray(function(err, users) {
+				for (var j = 0; j < users.length; j++) {
+					var user = users[j];
+					
+					for (var k = 0; k < user.posts.length; k++) {
+						var post = user.posts[k];
+						
+						var item = {
+							id: post._id.toString(),
+							username: user.username,
+							date: post.date,
+							title: post.title,
+							description: post.description,
+							route: post.route,
+							pics: post.pics
+						};
+						
+						result.push(item);
+					}
+				}
+			
+				res.send({
+					success : true,
+					posts: result,
+					msg : "OK",
+				});
+
+				db.close();
+			});
+		});
+	});
 });
 
 app.get('/:userToken/post/:postId', function(req, res){
